090dd1726a77fe3b091bf972da2227bd6343e8ce
Colin Knebl <colin.knebl@outlook.com>
2019-07-04 10:42:53 -0400

    fixed typo in comment and corrected misconfigured modulo calculation

--- modules/1/is_multiple.py
+++ modules/1/is_multiple.py
@@ -1,10 +1,10 @@
 def is_multiple(n, m):
     """
         Takes two integer values and returns True
-        is n is a multiple of m, that is, n = mi for
+        if n is a multiple of m, that is, n = mi for
         some integer i, and False otherwise.
     """
-    if m % n == 0:
+    if n % m == 0:
         return True
     else:
         return False
